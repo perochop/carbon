c640fa5cbc1dc00b27e17bd1de4d68c2221eff30
Michael Hogg <michael@kyan.com>
2019-11-22 14:08:39 +0000

    (bug-in-fixtures-log) Add debug code to demonstrate bug

--- fixtures/src/components/chrome.js
+++ fixtures/src/components/chrome.js
@@ -3,6 +3,13 @@ import Link from 'carbon-react/lib/components/link';
 import AppWrapper from 'carbon-react/lib/components/app-wrapper';
 import Log, { LogProvider } from './log';
 
+
+function debugPrintHistory(his, debugLabel) {
+  console.log(`%c${debugLabel} (length = ${his.length}):`, 'font-weight:bold; color:firebrick');
+  console.log('%c' + his.map(historyEntry => '  ' + JSON.stringify(historyEntry)).join('\n'), 'color:green');
+}
+
+
 const Chrome = ({ children }) => {
   const [history, setHistory] = useState([]);
 
@@ -11,14 +18,26 @@ const Chrome = ({ children }) => {
       name, id, value, checked
     }
   }, obj) => {
-    setHistory([...history, {
+
+    const debugTitle = `\nChrome.log() -- ${obj.method.padEnd(8)} -- ${JSON.stringify({ name, id, value, checked })}\n `;
+    console.group(debugTitle);
+
+    const newHistory = [...history, {
       ...obj,
       timestamp: Date.now(),
       name,
       id,
       value,
       checked
-    }]);
+    }];
+
+    debugPrintHistory(history, 'Current history');
+    debugPrintHistory(newHistory, 'New history');
+
+    setHistory(newHistory);
+
+    console.groupEnd(debugTitle);
+
   }, [history, setHistory]);
 
   return (
