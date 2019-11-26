aa8a41c996eb746ff045b1724f1fb104a41de08a
Ed Leeks <edward.leeks@sage.com>
2019-11-26 09:39:28 +0000

    remove console log

--- src/__experimental__/components/date/date.component.js
+++ src/__experimental__/components/date/date.component.js
@@ -131,7 +131,6 @@ class BaseDateInput extends React.Component {
       this.isBlurBlocked = true;
       return;
     }
-    console.log('called');
 
     this.updateSelectedDate(this.props.value || isoFormattedValueString(this.state.visibleValue));
     this.setState({ isDatePickerOpen: true });
