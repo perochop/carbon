9d5ba1b2c11f5517d51e2d481087001d780acbc9
dawidzarzycki <dzarzycki@pgs-soft.com>
2019-12-12 17:22:12 +0100

    Add `setTrapFocus` and `removeTrapFocus` util functions

---
+++ src/utils/helpers/focus-trap/focus-trap.js
@@ -0,0 +1,42 @@
+
+const tabbing = (ev, firstFocusableElement, lastFocusableElement) => {
+  if (ev.key === 'Tab' || ev.keyCode === 9) {
+    if (ev.shiftKey) /* shift + tab */ {
+      if (document.activeElement === firstFocusableElement) {
+        lastFocusableElement.focus();
+        ev.preventDefault();
+      }
+    } else if (document.activeElement === lastFocusableElement) {
+      firstFocusableElement.focus();
+      ev.preventDefault();
+    }
+  }
+};
+
+const setFocusTrap = (element) => {
+  // eslint-disable-next-line max-len
+  const ALL_FOCUSABLE_ELEMENTS = 'button, [href], input:not([type="hidden"]), select, textarea, [tabindex]:not([tabindex="-1"])';
+  const focusableElements = element.querySelectorAll(ALL_FOCUSABLE_ELEMENTS);
+
+  if (focusableElements.length <= 0) {
+    return;
+  }
+
+  const firstFocusableElement = focusableElements[0];
+  const lastFocusableElement = focusableElements[focusableElements.length - 1];
+
+
+  setTimeout(() => {
+    firstFocusableElement.focus();
+    document.addEventListener('keydown', ev => tabbing(ev, firstFocusableElement, lastFocusableElement));
+  });
+};
+
+const removeFocusTrap = () => {
+  document.removeEventListener('keydown', tabbing);
+};
+
+export {
+  setFocusTrap,
+  removeFocusTrap
+};
---
+++ src/utils/helpers/focus-trap/index.js
@@ -0,0 +1,2 @@
+// eslint-disable-next-line import/prefer-default-export
+export { setFocusTrap, removeFocusTrap } from './focus-trap';
