cba48894ca956a0072a9f37cbc0c1baa83d6ade8
dawidzarzycki <dzarzycki@pgs-soft.com>
2019-12-19 17:01:28 +0100

    feat(focus-trap): improve focus-trap util
    What has been changed:
    
    - setTimetout has been removed
    - better support for removed or added items

--- src/utils/helpers/focus-trap/focus-trap.js
+++ src/utils/helpers/focus-trap/focus-trap.js
@@ -1,10 +1,25 @@
+let element;
+let focusableElements;
+let firstFocusableElement;
+let lastFocusableElement;
+
 const blockTabbing = (ev) => {
   if (ev.keyCode === 9 || ev.keyCode === 16) {
     ev.preventDefault();
   }
 };
 
-const tabbing = (ev, firstFocusableElement, lastFocusableElement) => {
+// eslint-disable-next-line max-len
+const ALL_FOCUSABLE_ELEMENTS = 'button, [href], input:not([type="hidden"]), select, textarea, [tabindex]:not([tabindex="-1"])';
+
+const setFocusableItems = () => {
+  focusableElements = element.querySelectorAll(ALL_FOCUSABLE_ELEMENTS);
+  firstFocusableElement = focusableElements[0];
+  lastFocusableElement = focusableElements[focusableElements.length - 1];
+};
+
+const tabbing = (ev) => {
+  setFocusableItems();
   if (ev.key === 'Tab' || ev.keyCode === 9) {
     if (ev.shiftKey) /* shift + tab */ {
       if (document.activeElement === firstFocusableElement) {
@@ -18,23 +33,18 @@ const tabbing = (ev, firstFocusableElement, lastFocusableElement) => {
   }
 };
 
-const setFocusTrap = (element) => {
-  // eslint-disable-next-line max-len
-  const ALL_FOCUSABLE_ELEMENTS = 'button, [href], input:not([type="hidden"]), select, textarea, [tabindex]:not([tabindex="-1"])';
-  const focusableElements = element.querySelectorAll(ALL_FOCUSABLE_ELEMENTS);
+const setFocusTrap = (el) => {
+  element = el;
+
+  setFocusableItems();
 
   if (focusableElements.length <= 0) {
     document.addEventListener('keydown', blockTabbing);
     return;
   }
 
-  const firstFocusableElement = focusableElements[0];
-  const lastFocusableElement = focusableElements[focusableElements.length - 1];
-
-  setTimeout(() => {
-    firstFocusableElement.focus();
-    document.addEventListener('keydown', ev => tabbing(ev, firstFocusableElement, lastFocusableElement));
-  });
+  firstFocusableElement.focus();
+  document.addEventListener('keydown', ev => tabbing(ev));
 };
 
 const removeFocusTrap = () => {
