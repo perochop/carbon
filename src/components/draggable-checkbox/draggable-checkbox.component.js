60c5bd066cdf69ba469b1410dce0e2e616b41822
dawidzarzycki <dzarzycki@pgs-soft.com>
2020-02-07 16:10:50 +0100

    feat(draggable-checkbox): wip

--- src/components/draggable-checkbox/draggable-checkbox.component.js
+++ src/components/draggable-checkbox/draggable-checkbox.component.js
@@ -1,45 +1,79 @@
-import React from 'react';
-import { DndProvider, useDrag } from 'react-dnd';
+/* eslint-disable react/prop-types */
+import React, { useState } from 'react';
+import { DndProvider, useDrag, useDrop } from 'react-dnd';
 import Backend from 'react-dnd-html5-backend';
 
-const DraggableItem = ({ children }) => {
-  const [isDragging, dragRef] = useDrag({
-    item: { type: 'DraggableItem', children },
+const DraggableCheckboxItem = ({
+  id, children, findCard, moveCard
+}) => {
+  const originalIndex = findCard(id).index;
+  const [, drag] = useDrag({
+    item: { type: 'card', id, originalIndex },
     collect: monitor => ({
       isDragging: monitor.isDragging()
-    })
+    }),
+    end: (dropResult, monitor) => {
+      const { id: droppedId, originalIndex } = monitor.getItem();
+      const didDrop = monitor.didDrop();
+      if (!didDrop) {
+        moveCard(droppedId, originalIndex);
+      }
+    }
+  });
+  const [, drop] = useDrop({
+    accept: 'card',
+    canDrop: () => false,
+    hover({ id: draggedId }) {
+      if (draggedId !== id) {
+        const { index: overIndex } = findCard(id);
+        moveCard(draggedId, overIndex);
+      }
+    }
   });
 
-  console.log(isDragging);
-
   return (
-    <div ref={ dragRef }>
+    <div ref={ node => drag(drop(node)) }>
       {children}
     </div>
   );
 };
 
-const DraggableCheckbox = () => {
-  const [cards, setCards] = useState(ITEMS);
+const DropTarget = ({ children }) => {
+  const [, drop] = useDrop({ accept: 'card' });
+
+  return <div ref={ drop }>{children}</div>;
+};
+
+const DraggableCheckbox = ({ children }) => {
+  const [cards, setCards] = useState(children);
+
+  const findCard = (id) => {
+    const card = cards.filter(c => `${c.id}` === id)[0];
+    return {
+      card,
+      index: cards.indexOf(card)
+    };
+  };
+
   const moveCard = (id, atIndex) => {
     const { card, index } = findCard(id);
-    setCards(
-      update(cards, {
-        $splice: [
-          [index, 1],
-          [atIndex, 0, card]
-        ]
-      }),
-    );
+    setCards(...cards, {
+      $splice: [
+        [index, 1],
+        [atIndex, 0, card]
+      ]
+    });
   };
 
   return (
     <DndProvider backend={ Backend }>
-      <DraggableItem>
-    TEST
-      </DraggableItem>
+      <DropTarget>
+        {React.Children.map(children, (child) => {
+          return React.cloneElement(child, { findCard, moveCard });
+        })}
+      </DropTarget>
     </DndProvider>
   );
 };
 
-export default DraggableCheckbox;
+export { DraggableCheckbox, DraggableCheckboxItem };
--- src/components/draggable-checkbox/draggable-checkbox.stories.js
+++ src/components/draggable-checkbox/draggable-checkbox.stories.js
@@ -1,9 +1,16 @@
 import React from 'react';
-import DraggableCheckbox from './draggable-checkbox.component';
+import { DraggableCheckbox, DraggableCheckboxItem } from './draggable-checkbox.component';
 
 export default {
   component: DraggableCheckbox,
   title: 'DraggableCheckbox'
 };
 
-export const draggable = () => <DraggableCheckbox />;
+export const draggable = () => (
+  <DraggableCheckbox>
+    <DraggableCheckboxItem id={ 1 }> content 1</DraggableCheckboxItem>
+    <DraggableCheckboxItem id={ 2 }> content 1</DraggableCheckboxItem>
+    <DraggableCheckboxItem id={ 3 }> content 1</DraggableCheckboxItem>
+    <DraggableCheckboxItem id={ 4 }> content 1</DraggableCheckboxItem>
+  </DraggableCheckbox>
+);
