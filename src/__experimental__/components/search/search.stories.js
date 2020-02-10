76f58cf4d7d015bfb7090389cd84b9f24edcdbb0
Daniel Dipper <daniel.dipper@sage.com>
2020-02-10 14:16:41 +0000

    refactor(search): change storiesOf to CSF

--- src/__experimental__/components/search/search.stories.js
+++ src/__experimental__/components/search/search.stories.js
@@ -1,38 +1,26 @@
 import React from 'react';
-import { storiesOf } from '@storybook/react';
-import { boolean, text, number } from '@storybook/addon-knobs';
+import {
+  boolean,
+  text,
+  number,
+  withKnobs
+} from '@storybook/addon-knobs';
 import { Store, State } from '@sambego/storybook-state';
 import { action } from '@storybook/addon-actions';
-import { dlsThemeSelector } from '../../../../.storybook/theme-selectors';
 import { notes, info } from './documentation';
 import Search from '.';
-import getDocGenInfo from '../../../utils/helpers/docgen-info';
 
-Search.__docgenInfo = getDocGenInfo(
-  require('./docgenInfo.json'),
-  /search\.component(?!spec)/
-);
+export default {
+  title: 'Experimental/Search',
+  component: Search,
+  decorators: [withKnobs]
+};
 
-const store = new Store({
-  value: ''
-});
+export const searchComponent = () => {
+  const store = new Store({
+    value: ''
+  });
 
-function makeStory(name, themeSelector, component) {
-  const metadata = {
-    themeSelector,
-    info: {
-      text: info,
-      propTables: [Search],
-      propTablesExclude: [State]
-    },
-    notes: { markdown: notes },
-    knobs: { escapeHTML: false }
-  };
-
-  return [name, component, metadata];
-}
-
-const searchStory = () => {
   const handleChange = (ev) => {
     store.set({ value: ev.target.value });
     action('change')(ev);
@@ -43,20 +31,26 @@ const searchStory = () => {
   };
 
   return (
-    <State store={ store }>
-      <Search
-        placeholder={ text('placeholder', 'Search...') }
-        threshold={ number('threshold', Search.defaultProps.threshold) }
-        searchButton={ boolean('searchButton', true) }
-        onChange={ handleChange }
-        onBlur={ handleBlur }
-        value={ store.get('value') }
-        name='search_name'
-        id='search_id'
-      />
-    </State>
+    <Search
+      placeholder={ text('placeholder', 'Search...') }
+      threshold={ number('threshold', Search.defaultProps.threshold) }
+      searchButton={ boolean('searchButton', true) }
+      onChange={ handleChange }
+      onBlur={ handleBlur }
+      value={ store.get('value') }
+      name='search_name'
+      id='search_id'
+    />
   );
 };
 
-storiesOf('Experimental/Search', module)
-  .add(...makeStory('default', dlsThemeSelector, searchStory));
+searchComponent.story = {
+  name: 'default',
+  parameters: {
+    info: {
+      text: info,
+      propTablesExclude: [State]
+    },
+    notes: { markdown: notes }
+  }
+};
